f235a72d4e54284cbf325fafc8470c90b798f982
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-11 05:58:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from PIL import Image
 
+uploaded_files = st.file_uploader("Choose an image file", accept_multiple_files=False)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+
+image = Image.open(uploaded_file.name)
+
+st.image(image, caption='Sunrise by the mountains')
