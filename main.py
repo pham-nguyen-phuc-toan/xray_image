39766f40704709d76196ddb53ac083b1a204b4d5
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-11 06:11:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ from PIL import Image
 uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False)
 if uploaded_file is not None:
     st.write("filename:", uploaded_file.name)
-
     image = Image.open(uploaded_file)
-    
-    st.image(image, caption='Sunrise by the mountains')
+    st.image(image, caption='Test image')
