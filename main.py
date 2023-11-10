3d5fa15102a9ad599600afe0cd237541bc1d2af6
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-11 06:09:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from PIL import Image
 
 uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False)
 if uploaded_file is not None:
-    bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
 
-    image = Image.open(uploaded_file.name)
+    image = Image.open(uploaded_file)
     
     st.image(image, caption='Sunrise by the mountains')
