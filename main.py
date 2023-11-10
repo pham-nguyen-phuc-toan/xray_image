01d4ae7be54b4b3291e70746d0fcf2abe32606ec
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-11 06:06:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=F
 if uploaded_file is not None:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
 
     image = Image.open(uploaded_file.name)
     
