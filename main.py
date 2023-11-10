e2bc25db6e8ffc1a9a4d4f1ca53c523fd354d3b3
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-11 06:04:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-uploaded_files = st.file_uploader("Choose an image file", accept_multiple_files=False)
+uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False)
 if uploaded_file is not None:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
