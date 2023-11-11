0b3b6148d308c537548eed20f9b21649b6f58b47
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-11 08:44:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from PIL import Image
+from joblib import load
 
 uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False)
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Test image')
+
+model = load('lrc_xray.joblib')
