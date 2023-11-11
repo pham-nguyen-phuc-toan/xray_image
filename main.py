41f149642a92c9e033464428352cd7dfd94b0bce
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-11 11:09:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 from PIL import Image
 from joblib import load
 
+IMG_SIZE = 227
+
 model = load('lrc_xray.joblib')
 
 uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False)
@@ -10,4 +12,5 @@ if uploaded_file is not None:
     st.image(image, caption='Test image')
 
     if st.button('Predict'):
-        st.write(model.predict(st.image))
+        image = image.reshape(1, IMG_SIZE*IMG_SIZE*3)
+        st.write(model.predict(image))
