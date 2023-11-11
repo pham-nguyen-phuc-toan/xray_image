53d04cedf8d352d7f8d5b4932a76b5c4b60b55ea
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-11 08:46:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import streamlit as st
 from PIL import Image
 from joblib import load
 
+model = load('lrc_xray.joblib')
+
 uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False)
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Test image')
 
-model = load('lrc_xray.joblib')
+    if st.button('Predict'):
+        st.write(model.predict(st.image)
