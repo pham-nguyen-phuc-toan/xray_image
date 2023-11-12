ca8985037c28babf0c3fad87840548f6e30423a3
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-12 09:29:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if uploaded_file is not None:
 
     if st.button('Predict'):
         image = image.resize((1, IMG_SIZE*IMG_SIZE*3))
-        st.write(model.predict(image))
+        label = model.predict(image)
+        st.write(label)
