79afc044e0a98aa0854cb548bd65601d5e249b07
pham-nguyen-phuc-toan <55023095+pham-nguyen-phuc-toan@users.noreply.github.com>
2023-11-12 10:19:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import streamlit as st
-from PIL import Image
-import pickle
+import cv2 as cv
+import pickle as pkl
 
 IMG_SIZE = 227
 
 input = open('lrc_xray.pkl', 'rb')
-model = pickle.load(input)
+model = pkl.load(input)
 
 uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    image = cv.imread(uploaded_file)
     st.image(image, caption='Test image')
 
     if st.button('Predict'):
